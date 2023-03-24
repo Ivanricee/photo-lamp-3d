583a2c0441ac044a3493e16741db8faff8e372da
Iván Bautista <ivanbautistag@gmail.com>
2023-03-23 18:21:57 -0600

    Fix: remove title prop in header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,7 @@ export function Layout({ children, title }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header title={title} />
+      <Header />
       <main>{children}</main>
     </>
   )
