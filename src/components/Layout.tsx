5e315d4e21a1a4cc929e4b74da9610ebdf965114
Iván Bautista <ivanbautistag@gmail.com>
2023-03-29 15:07:01 -0600

    Fix: not show header in home landing page, wih header prop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,18 +4,22 @@ import { Header } from './Header'
 interface Props {
   children: React.ReactNode
   title: string
+  header: boolean
 }
 
-export function Layout({ children, title }: Props) {
+export function Layout({ children, title, header }: Props) {
+  const item = header ? 'items-start' : 'items-center'
   return (
-    <>
+    <div className="flex flex-col min-h-screen max-h-full w-screen min-w-[361px] bg-gradient-body bg-no-repeat">
       <Head>
-        <title>{title}</title>
+        <title>Photo Lamp 3D - {title}</title>
         <meta name="description" content="3D lamp generator" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>{children}</main>
-    </>
+
+      {header && <Header />}
+      <main className={`flex flex-1 justify-center ${item}`}>{children}</main>
+    </div>
   )
 }
