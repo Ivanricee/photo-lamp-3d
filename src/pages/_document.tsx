b2f0dbf340083588ecea7f41b4b0070157559641
Iván Bautista <ivanbautistag@gmail.com>
2023-03-29 15:07:34 -0600

    fix: remove styles in body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="min-w-[361px]">
+      <body>
         <Main />
         <NextScript />
       </body>
