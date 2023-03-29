5756b86d55b53effc9adb040f14453baef969c61
Iván Bautista <ivanbautistag@gmail.com>
2023-03-29 15:02:15 -0600

    Feat: add simple header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
-import Image from 'next/image'
-
 export function Header() {
-  return <header>PhotLamp3D</header>
+  return (
+    <header className="self-start flex gap-5 text-green-100 m-8 xs:m-4">
+      <div
+        className="text-sm rounded-xl text-green-100 p-2 pb-3 px-4 drop-shadow-5xl bg-green-gray-600
+                   sm:drop-shadow-5xl md:drop-shadow-6xl"
+      >
+        {'<--'}
+      </div>
+      <h1
+        className="text-sm font-bold rounded-xl text-green-100  p-2 pb-3 px-4 drop-shadow-5xl bg-green-gray-600
+                   sm:drop-shadow-5xl md:drop-shadow-6xl"
+      >
+        <span className="font-medium">PHOTO</span> LAMP 3D
+      </h1>
+    </header>
+  )
 }
