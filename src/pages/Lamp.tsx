5ff2b7e9412cc57770ca4bb4181f2e8a566dc488
Iván Bautista <ivanbautistag@gmail.com>
2023-04-06 18:13:05 -0600

    feat/fix get data from server

--- src/pages/Lamp.tsx
+++ src/pages/Lamp.tsx
@@ -1,23 +1,87 @@
+import { Button } from '@/components/Button'
 import { Layout } from '@/components/Layout'
+import { useState } from 'react'
+import { type Lamp } from '@/types/types'
+import { Card } from '@/components/Card'
 
-export default function Lamp() {
+type ListOfLamp = Lamp[]
+
+interface Props {
+  listOfLamps: ListOfLamp
+}
+/**
+
+ */
+export default function Lamp({ listOfLamps }: Props) {
+  const [lampPicked, setlampPicked] = useState<Lamp>({
+    id: null,
+    title: null,
+    canvasUrl: null,
+    description: 'Chose your favorite lamp',
+  })
+  const handleLampClick = (lampItem: Lamp) => {
+    setlampPicked((lamp) => ({ ...lamp, ...lampItem }))
+  }
+
   return (
-    <Layout title="Lamp" header>
-      <section
-        className="w-full flex flex-col bg-gradient-card-2 rounded-3xl drop-shadow-dark-4xl m-8 p-8 text-center
-        xs:m-4 sm:text-left sm:max-w-7xl"
-      >
-        <h2 className="w-full text-green-200 text-center text-xl">
-          CHOSE A LAMP
-        </h2>
-        <div className="w-full relative flex overflow-auto xs:h-40 h-56 items-center  top-0 bottom-0">
-          <div className="absolute flex flex-nowrap gap-8  justify-center items-center xs:gap-4">
-            <div className="xs:w-28 w-40 aspect-square border border-green-300"></div>
-            <div className="xs:w-28 w-40 aspect-square border border-green-300"></div>
-          </div>
+    <Layout title="Lamp" header={true}>
+      <Card title="CHOSE A LAMP">
+        <ul
+          className="w-full relative flex overflow-auto xs:h-40 h-56 items-center  top-0 bottom-0"
+          role="list"
+          aria-label="list of lamps"
+        >
+          <li className="absolute flex flex-nowrap gap-8  justify-center items-center xs:gap-4">
+            {listOfLamps.map((lamp) => {
+              return (
+                <button
+                  onClick={() => handleLampClick(lamp)}
+                  key={lamp.id}
+                  className="xs:w-28 w-40 aspect-square border border-green-300"
+                >
+                  {lamp.title}
+                </button>
+              )
+            })}
+          </li>
+        </ul>
+        <div
+          className="w-full aspect-video border border-green-300"
+          aria-label="canvas 3D lamp"
+        >
+          {lampPicked.description}
         </div>
-        <div className="w-full aspect-video border border-green-300"></div>
-      </section>
+        <div className="mt-8 text-center">
+          <Button
+            to={`/Photograph/${lampPicked.id}`}
+            disabled={!lampPicked.id ? true : false}
+          >
+            Next step
+          </Button>
+        </div>
+      </Card>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const listOfLamps: ListOfLamp = await [
+    {
+      id: 1,
+      title: 'marco tipo red social',
+      canvasUrl: 'img provisional 1',
+      description: 'a',
+    },
+    {
+      id: 2,
+      title: 'frame crystal',
+      canvasUrl: 'img provisional 2',
+      description: 'b',
+    },
+  ]
+  return {
+    props: {
+      listOfLamps,
+    },
+  }
 }
