449768590a50d78613314512dbe24ba614bda015
Iván Bautista <ivanbautistag@gmail.com>
2023-04-06 18:15:36 -0600

    fix: margin and padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="self-start flex gap-5 text-green-100 m-8 xs:m-4">
+    <header className="self-center flex gap-5 text-green-100 m-8 xs:m-4 w-full px-8 xs:px-4 sm:max-w-[84rem]">
       <div
         className="text-sm rounded-xl text-green-100 p-2 pb-3 px-4 drop-shadow-5xl bg-green-gray-600
                    sm:drop-shadow-5xl md:drop-shadow-6xl"
