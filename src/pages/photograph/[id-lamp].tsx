b6f4627ccc0e5e8efc1a4af16ccd5c3065645f35
Iván Bautista <ivanbautistag@gmail.com>
2023-04-06 18:10:58 -0600

    Add photograph file to get an id lamp

---
+++ src/pages/photograph/[id-lamp].tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/Card'
+import { Layout } from '@/components/Layout'
+
+export default function Photograph() {
+  return (
+    <Layout title="photgraph" header={true}>
+      <Card title="UPLOAD YOUR PHOTO">
+        <div>photo</div>
+      </Card>
+    </Layout>
+  )
+}
