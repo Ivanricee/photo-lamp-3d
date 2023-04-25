c91edcce3dfd06bc06958f70e74172ce60bd93b3
Iván Bautista <ivanbautistag@gmail.com>
2023-04-25 00:10:40 -0600

    feat: add 3D canvas to landing page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import { Button } from '@/components/Button'
 import { Layout } from '@/components/Layout'
+import { Landing3DFrames } from '@/components/Three/Landing3DFrame/Index'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <Layout title="Home" header={false}>
-      <section className="sm:max-w-[768px] bg-gradient-card-1 rounded-3xl drop-shadow-4xl m-8 p-8 xs:m-4 text-center sm:text-left">
-        <div className="flex flex-wrap justify-center sm:flex-nowrap">
-          <div className="w-full px-12 xs:px-4 sm:px-0 sm:w-3/5 sm:pr-16">
+      <section
+        className="inline-block w-full bg-gradient-card-1 drop-shadow-4xl rounded-3xl m-8 p-8 text-center
+      sm:text-left md:max-w-[1024px] overflow-hidden"
+      >
+        <div
+          className="flex flex-wrap justify-center items-center
+        xs-m-4 sm:flex-nowrap sm:w-full"
+        >
+          <div className="w-full px-12 xs:px-4 sm:px-0 sm:pr-8 md:px-16">
             <h1
               className="text-3xl mb-1 font-bold text-green-100 border-st
                          xs:text-xl"
@@ -31,8 +38,13 @@ export default function Home() {
               </Button>
             </div>
           </div>
-          <div className="w-4/5 flex-grow-1 aspect-video border border-green-100 xs:w-full sm:w-2/5">
-            <p>three js section</p>
+          <div
+            className="w-full h-[31rem] flex-grow-1  border border-green-100 rounded-3xl overflow-hidden
+          xs:w-full"
+          >
+            <div className="w-full ">
+              <Landing3DFrames />
+            </div>
           </div>
         </div>
       </section>
