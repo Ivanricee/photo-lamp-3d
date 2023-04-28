4b281f3973481fd9e2560c67b9ffa773dc1baa29
Iván Bautista <ivanbautistag@gmail.com>
2023-04-27 23:51:16 -0600

    Feat: add urlMesh to interface Lamp. new types  idLamp, MenuHomeSlice for zustand store home

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,14 @@
 export interface Lamp {
-  id: number | null
-  title: string | null
-  canvasUrl: string | null
-  description: string | null
+  id: number
+  img: string
+  alt: string
+  size: number
+  caption?: string | null
+  urlMesh: string
+}
+export type idLamp = typeof Pick<Lamp, 'id'>
+//zustand
+export interface menuHomeSlice {
+  lampHome: idLamp
+  setLamp: (lamp: idLamp) => void
 }
