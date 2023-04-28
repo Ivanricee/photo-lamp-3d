f7a780612f4073d085601cbb5753ed082b201d34
Iván Bautista <ivanbautistag@gmail.com>
2023-04-28 00:27:46 -0600

    add: const used in the app

---
+++ src/consts/index.js
@@ -0,0 +1,10 @@
+export const MENU_SIZE = Object.freeze({
+  SMALL: Symbol(),
+  LARGE: Symbol(),
+})
+
+export const MENU_SELECT = Object.freeze({
+  HOME: Symbol(),
+  MAIN: Symbol(),
+  PHOTO: Symbol(),
+})
