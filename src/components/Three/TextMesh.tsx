984138380abefa6aa2875daba90accf350b1102a
Iván Bautista <ivanbautistag@gmail.com>
2023-05-07 15:25:38 -0600

    refactor: remove manual position, use props for dynamic position.

--- src/components/Three/TextMesh.tsx
+++ src/components/Three/TextMesh.tsx
@@ -3,17 +3,19 @@ import { Center, Text3D } from '@react-three/drei'
 interface Props {
   title: string
   caption: string
+  position: [number, number, number]
+  rotation: [number, number, number]
 }
-export function TextMesh({ title, caption }: Props) {
+export function TextMesh({ title, caption, position, rotation }: Props) {
   return (
-    <group>
-      <Center top left position={[0.1, -1.45, -0.73]} rotation={[0, 0.363, 0]}>
+    <group position={position} rotation={rotation}>
+      <Center top right>
         <Text3D font="fonts/Merienda_Regular.json" size={0.09} height={0.1}>
           {title}
           <meshPhysicalMaterial attach="material" color="#2f411e" />
         </Text3D>
       </Center>
-      <Center top left position={[0.92, -1.62, -1.03]} rotation={[0, 0.363, 0]}>
+      <Center top right position={[0, -0.2, 0]}>
         <Text3D font="fonts/Merienda_Regular.json" size={0.072} height={0.1}>
           {caption}
           <meshPhysicalMaterial attach="material" color="#364b23" />
