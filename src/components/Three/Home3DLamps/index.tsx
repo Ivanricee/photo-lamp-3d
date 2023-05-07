f504f2514875da2ccf09824e4d75b20331d6bfc2
Iván Bautista <ivanbautistag@gmail.com>
2023-05-07 15:22:06 -0600

    feat: Fetch home menu, first item & lamp gltf, store locally.

--- src/components/Three/Home3DLamps/index.tsx
+++ src/components/Three/Home3DLamps/index.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import {
   AdaptiveDpr,
   BakeShadows,
@@ -12,45 +12,52 @@ import {
   EffectComposer,
   HueSaturation,
 } from '@react-three/postprocessing'
-import { Frame } from './Frame'
 import { Backdrop } from './Backdrop'
 import { LoaderCanvas } from '../LoaderCanvas'
 import { Menu } from '@/components/Menu'
 import { MENU_SELECT, MENU_SIZE } from '@/consts'
-import { type Lamp } from '@/types/types'
 import { useBoundStore } from '@/stores/useBoundStore'
+import { GLBLoader } from '../GLBLoader'
+import { type gltfData } from '@/types/types'
 
-interface Props {
-  lamps: Lamp[]
-}
-export function Home3DLamps({ lamps }: Props) {
-  const lampHome = useBoundStore((state) => state.lampHome)
-  console.log({ lampHome })
+export function Home3DLamps() {
+  const currentLampHomeId = useBoundStore((state) => state.currentLampHomeId)
+  //menu item
+  const homeLamps = useBoundStore((state) => state.homeLamps)
+  const [gltfLampData, setGltfLampData] = useState<gltfData | null>(null)
+
+  useEffect(() => {
+    if (currentLampHomeId && homeLamps) {
+      const lampSelected = homeLamps?.find(
+        (lamp) => lamp.id === currentLampHomeId
+      )
+      setGltfLampData({
+        urlFile: lampSelected?.urlFile,
+        text3DTest: { ...lampSelected?.text3DTest },
+      })
+    }
+  }, [currentLampHomeId])
+
   return (
-    <div className="w-full h-[500px] relative">
-      <Suspense fallback={<LoaderCanvas caption="Loading 3D Frame" />}>
-        <Menu
-          direction="flex-col"
-          gap="gap-3"
-          hasBackground={false}
-          menuItems={lamps}
-          size={MENU_SIZE.SMALL}
-          type={MENU_SELECT.HOME}
-        />
-
-        <Canvas
-          text-aria-label="3d viewport"
-          shadows
-          dpr={[1, 2]}
-          //frameloop="demand"
-        >
-          <PerspectiveCamera
-            makeDefault
-            position={[-1, 0.5, 1.5]}
-            fov={35}
-            castShadow={true}
+    <div className="w-full xs:h-[370px] h-[500px] sm:h-[330px] md:h-[500px] relative">
+      {currentLampHomeId && homeLamps && (
+        <Suspense fallback={<LoaderCanvas caption="Loading 3D Frame" />}>
+          <Menu
+            direction="flex-col"
+            gap="gap-3"
+            hasBackground={false}
+            menuItems={homeLamps}
+            size={MENU_SIZE.SMALL}
+            type={MENU_SELECT.HOME}
           />
-          {/*
+
+          <Canvas
+            text-aria-label="3d viewport"
+            shadows
+            dpr={[1, 2]}
+            //frameloop="demand"
+          >
+            {/*
             rota en un solo eje
             <OrbitControls
             enableZoom={false}
@@ -58,33 +65,48 @@ export function Home3DLamps({ lamps }: Props) {
             minPolarAngle={Math.PI / 2}
             maxPolarAngle={Math.PI / 2}
           />*/}
-          <BakeShadows />
-          <Frame />
-          <ambientLight intensity={0.2} />
-          <Backdrop />
-          <AdaptiveDpr pixelated />
-          <EffectComposer>
-            <Bloom
-              intensity={1.5}
-              luminanceThreshold={0.38}
-              luminanceSmoothing={1}
-              height={200}
-            />
-            <HueSaturation saturation={0.2} hue={0.1} />
-          </EffectComposer>
-          <ContactShadows
-            position={[0, -0.8, 0]}
-            opacity={0.25}
-            scale={10}
-            blur={1.5}
-            far={0.8}
+            <group position={[0, -1.8, 0]}>
+              <PerspectiveCamera
+                makeDefault
+                position={[-1, 10.5, 1.5]}
+                fov={35}
+                castShadow={true}
+              />
+              {gltfLampData && (
+                <GLBLoader
+                  urlFile={gltfLampData.urlFile}
+                  text3DTest={gltfLampData.text3DTest}
+                />
+              )}
+
+              <ambientLight intensity={0.3} />
+              <Backdrop />
+              <AdaptiveDpr pixelated />
+              <EffectComposer>
+                <Bloom
+                  intensity={1.6}
+                  luminanceThreshold={0.38}
+                  luminanceSmoothing={1.5}
+                  height={100}
+                />
+                <HueSaturation saturation={0.3} hue={0.1} />
+              </EffectComposer>
+              <BakeShadows />
+              <ContactShadows
+                position={[0, -2, 0]}
+                opacity={0.5}
+                scale={10}
+                blur={1.5}
+                far={0.8}
+              />
+            </group>
+          </Canvas>
+          <Loader
+            containerStyles={{ background: '#262726' }}
+            dataStyles={{ fontSize: '1rem', color: '#D5FFCC' }}
           />
-        </Canvas>
-        <Loader
-          containerStyles={{ background: '#262726' }}
-          dataStyles={{ fontSize: '1rem', color: '#D5FFCC' }}
-        />
-      </Suspense>
+        </Suspense>
+      )}
     </div>
   )
 }
