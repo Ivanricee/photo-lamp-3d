ab5c184c6be38f444cddac17e4eca820cc957d12
Iván Bautista <ivanbautistag@gmail.com>
2023-05-07 15:09:00 -0600

    feat: add text 3D types and change zustand types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,14 +1,35 @@
+export interface text3DTest {
+  id: number
+  title: string
+  caption: string
+  position: [number, number, number]
+  rotation: [number, number, number]
+}
 export interface Lamp {
   id: number
   img: string
   alt: string
   size: number
-  caption?: string | null
-  urlMesh: string
+  caption: string | null
+  urlFile: string
+  text3DTest: text3DTest | null
 }
 export type idLamp = typeof Pick<Lamp, 'id'>
+export type urlFile = typeof Pick<Lamp, 'urlFile'>
+export type menuId = idLamp | number
+export type gltfData = typeof Pick<Lamp, 'id' | 'text3DTest'>
 //zustand
 export interface menuHomeSlice {
-  lampHome: idLamp
-  setLamp: (lamp: idLamp) => void
+  homeLamps: Lamp[] | null
+  currentLampHomeId: menuId
+  setLamps: (lamp: Lamp[]) => void
+  selectMenuHome: (selectedMenuItemHomeId: menuId) => void
+}
+export interface GLTFscene {
+  uuid?: string | null
+  scene?: THREE.Scene | null
+}
+export interface GLTFsceneSlice {
+  currentScenes: GLTFscene[]
+  addLampHomeScene: (addScene: GLTFscene) => void
 }
