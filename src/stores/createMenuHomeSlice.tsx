59157b6631516ce98c2ef908f9610f7fbe14d949
Iván Bautista <ivanbautistag@gmail.com>
2023-05-07 15:11:03 -0600

    Fix: change, store only the id of the menu home, and init with null

--- src/stores/createMenuHomeSlice.tsx
+++ src/stores/createMenuHomeSlice.tsx
@@ -1,14 +1,13 @@
 import { StateCreator } from 'zustand'
+
 import { type menuHomeSlice } from '@/types/types'
 
 export const createMenuHomeSlice: StateCreator<menuHomeSlice> = (set) => ({
-  lampHome: { id: 1, urlMesh: '' },
-  setLamp: (lampHome) =>
-    set((state) => ({
-      lampHome: {
-        ...state.lampHome,
-        id: lampHome.id,
-        urlMesh: lampHome.urlMesh,
-      },
-    })),
+  homeLamps: null,
+  currentLampHomeId: null,
+  setLamps: (lamps) => set({ homeLamps: lamps }),
+  selectMenuHome: (selectedMenuItemHomeId) =>
+    set({
+      currentLampHomeId: selectedMenuItemHomeId,
+    }),
 })
