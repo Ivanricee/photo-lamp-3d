6e109ae01cd5765071b0ef5b5cacd0d576094cdb
Iván Bautista <ivanbautistag@gmail.com>
2023-05-10 21:37:45 -0600

    fix: rename file

---
+++ src/stores/createMenuHomeSlice.ts
@@ -0,0 +1,13 @@
+import { StateCreator } from 'zustand'
+
+import { type menuHomeSlice } from '@/types/types'
+
+export const createMenuHomeSlice: StateCreator<menuHomeSlice> = (set) => ({
+  homeLamps: null,
+  currentLampHomeId: null,
+  setHomeLamps: (lamps) => set({ homeLamps: lamps }),
+  selectMenuHome: (selectedMenuItemHomeId) =>
+    set({
+      currentLampHomeId: selectedMenuItemHomeId,
+    }),
+})
