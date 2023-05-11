7baa9e0bf61680aa39d39e93b5dae84a4ad4a611
Iván Bautista <ivanbautistag@gmail.com>
2023-05-11 12:18:54 -0600

    Fix: remove margin from the classname

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,8 @@ interface Props {
 export function Card({ children, title }: Props) {
   return (
     <section
-      className="w-full flex flex-col bg-gradient-card-2 rounded-3xl drop-shadow-dark-4xl m-8 p-8 text-center
-    xs:m-4 sm:text-left sm:max-w-7xl"
+      className="w-full flex flex-col bg-gradient-card-2 rounded-3xl drop-shadow-dark-4xl  p-8 text-center
+    sm:text-left sm:max-w-7xl"
       aria-label={title}
     >
       <h2 className="w-full text-green-200 text-center text-xl">{title}</h2>
