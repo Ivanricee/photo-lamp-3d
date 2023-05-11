2eab941749f92c8450a73f40d3529600d92fb49d
Iván Bautista <ivanbautistag@gmail.com>
2023-05-10 21:35:49 -0600

    feat: new types for zustand stores

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -14,22 +14,42 @@ export interface Lamp {
   urlFile: string
   text3DTest: text3DTest | null
 }
+export interface LampMenu {
+  id: number
+  title: string
+  canvasUrl: string
+  description: string
+}
+export interface progressBar {
+  id: number
+  step: symbol
+}
 export type idLamp = typeof Pick<Lamp, 'id'>
 export type urlFile = typeof Pick<Lamp, 'urlFile'>
 export type menuId = idLamp | number
 export type gltfData = typeof Pick<Lamp, 'id' | 'text3DTest'>
-//zustand
-export interface menuHomeSlice {
-  homeLamps: Lamp[] | null
-  currentLampHomeId: menuId
-  setLamps: (lamp: Lamp[]) => void
-  selectMenuHome: (selectedMenuItemHomeId: menuId) => void
-}
 export interface GLTFscene {
   uuid?: string | null
   scene?: THREE.Scene | null
 }
+//zustand
+export interface progressSilce {
+  progressStep: progressBar[]
+  currentProgressStep: progressBar
+  addProgressBarItem: (progressItems: progressBar) => void
+}
+export interface menuHomeSlice {
+  homeLamps: Lamp[] | null
+  currentLampHomeId: menuId
+  setHomeLamps: (lamp: Lamp[]) => void
+  selectMenuHome: (selectedMenuItemHomeId: menuId) => void
+}
 export interface GLTFsceneSlice {
   currentScenes: GLTFscene[]
   addLampHomeScene: (addScene: GLTFscene) => void
+}
+
+export interface lampsSlice {
+  lamps: LampMenu[] | null
+  addLamps: (lamp: LampMenu[]) => void
 }
