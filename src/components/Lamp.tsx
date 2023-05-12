cf6cd090a74f3472067acbb81f3e90aebb08531b
Iván Bautista <ivanbautistag@gmail.com>
2023-05-11 23:26:40 -0600

    refactor: remove previous menu, add component <menu>

--- src/components/Lamp.tsx
+++ src/components/Lamp.tsx
@@ -1,53 +1,38 @@
 import { useBoundStore } from '@/stores/useBoundStore'
-import { type LampMenu } from '@/types/types'
+import { type Lamp } from '@/types/types'
 import { useState } from 'react'
 import { Button } from './Button'
 import { Card } from './Card'
+import { Menu } from './Menu'
+import { MENU_SELECT, MENU_SIZE } from '@/const'
 
 export function Lamp() {
   const lamps = useBoundStore((state) => state.lamps)
-  const [lampPicked, setlampPicked] = useState<LampMenu>({
-    id: 0,
-    title: '',
-    canvasUrl: '',
-    description: 'Chose your favorite lamp',
-  })
-  const handleLampClick = (lampItem: LampMenu) => {
+
+  const [lampPicked, setlampPicked] = useState<Lamp | null>(null)
+
+  const handleLampClick = (lampItem: Lamp) => {
     setlampPicked((lamp) => ({ ...lamp, ...lampItem }))
   }
+
   return (
     <Card title="CHOSE A LAMP">
-      <ul
-        className="w-full relative flex overflow-auto xs:h-40 h-56 items-center  top-0 bottom-0"
-        role="list"
-        aria-label="list of lamps"
-      >
-        <li className="absolute flex flex-nowrap gap-8  justify-center items-center xs:gap-4">
-          {lamps ? (
-            lamps.map((lamp) => {
-              return (
-                <button
-                  onClick={() => handleLampClick(lamp)}
-                  key={lamp.id}
-                  className="xs:w-28 w-40 aspect-square border border-green-300"
-                >
-                  {lamp.title}
-                </button>
-              )
-            })
-          ) : (
-            <h1>loading</h1>
-          )}
-        </li>
-      </ul>
+      <Menu
+        direction="flex-row"
+        gap="gap-3"
+        hasBackground={false}
+        menuItems={lamps}
+        size={MENU_SIZE.LARGE}
+        type={MENU_SELECT.LAMP}
+      />
 
       <div className="mt-8 text-center">
-        <Button
+        {/*<Button
           to={`/Photograph/${lampPicked.id}`}
           disabled={!lampPicked.id ? true : false}
         >
           Next step
-        </Button>
+        </Button>*/}
       </div>
     </Card>
   )
