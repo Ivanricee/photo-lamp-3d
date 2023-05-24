3bb5c7ded7198ce4510cfae78d7d46bb25f31de4
Iván Bautista <ivanbautistag@gmail.com>
2023-05-24 13:19:59 -0600

    Feat: BTN_TYPE object

--- src/const.ts
+++ src/const.ts
@@ -5,14 +5,20 @@ export const MENU_SIZE = Object.freeze({
 
 export const MENU_SELECT = Object.freeze({
   HOME: Symbol(),
+  LAMP: Symbol(),
   PHOTO: Symbol(),
   LAYER: Symbol(),
 })
 
-export const PROGRESS_BAR = Object.freeze({
+export const BTN_TYPE = Object.freeze({
+  APP: Symbol(),
   LAMP: Symbol(),
-  IMG: Symbol(),
-  TEXT: Symbol(),
-  LAYER_1: Symbol(),
-  LAYER_2: Symbol(),
+})
+
+export const PROGRESS_BAR = Object.freeze({
+  LAMP: 'lamp',
+  IMG: 'img',
+  TEXT: 'text',
+  LAYER_1: 'layer1',
+  LAYER_2: 'layer2',
 })
