1c0fca47b4cd3dda0dca0b4e8b5815aa6db67a1d
Iván Bautista <ivanbautistag@gmail.com>
2023-05-24 13:05:40 -0600

    feat: progressBar step IMG new compoennt ImageLamp

---
+++ src/components/ImageLamp.tsx
@@ -0,0 +1,9 @@
+import { Card } from './Card'
+
+export function ImageLamp() {
+  return (
+    <Card title="Chose an Image">
+      <h1>Upload your phot and chjose to remove a bckgroudn</h1>
+    </Card>
+  )
+}
--- src/components/LampControls.tsx
+++ src/components/LampControls.tsx
@@ -1,14 +1,16 @@
 import { useBoundStore } from '@/stores/useBoundStore'
 import { Lamp } from './Lamp'
 import { PROGRESS_BAR } from '@/const'
+import { ImageLamp } from './ImageLamp'
 
 export function LampControls() {
   const currentProgressStep = useBoundStore(
     (state) => state.currentProgressStep
   )
   const { LAMP, IMG, TEXT, LAYER_1, LAYER_2 } = PROGRESS_BAR
-  const progressSteps = {
+  const progressSteps: { [key: string]: JSX.Element } = {
     [LAMP]: <Lamp />,
+    [IMG]: <ImageLamp />,
   }
   const currentStep = progressSteps[currentProgressStep.step]
 
