fcc3435e074a6f49a88dd9b40548f84e19fc0cbf
Iván Bautista <ivanbautistag@gmail.com>
2023-05-24 12:18:52 -0600

    refactor: progressBar now is part of lamp data

--- src/stores/useBoundStore.tsx
+++ src/stores/useBoundStore.tsx
@@ -1,20 +1,17 @@
 import { create } from 'zustand'
 import { createMenuHomeSlice } from './createMenuHomeSlice'
 import {
-  type progressSilce,
   type GLTFsceneSlice,
   type menuHomeSlice,
   lampsSlice,
 } from '@/types/types'
 import { create3DSceneSlice } from './create3DSceneSlice'
-import { createProgressSlice } from './createProgressSlice'
 import { createLampSlice } from './createLampSlice'
 
 export const useBoundStore = create<
-  menuHomeSlice & GLTFsceneSlice & progressSilce & lampsSlice
+  menuHomeSlice & GLTFsceneSlice & lampsSlice
 >()((...a) => ({
   ...createMenuHomeSlice(...a),
   ...create3DSceneSlice(...a),
-  ...createProgressSlice(...a),
   ...createLampSlice(...a),
 }))
